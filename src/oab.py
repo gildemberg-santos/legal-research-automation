26851f9ff1d4ba3f47ca9386723cc10e8e343990
Gildemberg Santos <gildemberg.santos@gmail.com>
2022-07-31 10:28:43 -0300

    Requisições http

--- src/oab.py
+++ src/oab.py
@@ -9,3 +9,12 @@ class Oab:
         else:
             data = None
         return data
+
+    def post_oab_data(self, endpoint:str, data:dict) -> dict:
+        self.url = f'{HOST}/{endpoint}'
+        response = requests.post(self.url, json=data)
+        if response.status_code == 200:
+            data = response.json()
+        else:
+            data = {'error': 'Unable to complete the request'}
+        return data
